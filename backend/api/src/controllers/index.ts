ca6be0ee65688162ca5615e2919ed3dbcf6af4a9
Quentin <quentindumon352a@gmail.com>
2025-04-23 20:14:23 +0200

    feat(S:Controllers)!: add index.ts for export all controllers
    Refs: #24

---
+++ backend/api/src/controllers/index.ts
@@ -0,0 +1,6 @@
+// Usser controllers
+import { default as createUser } from "@controllers/user/createUser.controller.ts"
+// Activity controllers
+// Infroamtion controllers
+
+export default createUser
