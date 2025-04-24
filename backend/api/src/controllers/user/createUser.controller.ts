a3933db2d24fe91978e4b47b343a316857bc083a
Quentin <quentindumon352a@gmail.com>
2025-04-24 11:08:10 +0200

    feat(C:Users): update createUser controller to encrypt sensitive data and return decrypted data
    Refs: #24

--- backend/api/src/controllers/user/createUser.controller.ts
+++ backend/api/src/controllers/user/createUser.controller.ts
@@ -1,12 +1,13 @@
-import type { IUserReqBodyRequest } from "@api/types/user.d.ts"
 import { errorHandler, handleUnexpectedError } from "@errorHandler/errorHandler.ts"
-import { ERROR_CODE } from "@errorHandler/configs.errorHandler.ts"
+import { validateRequiredUserFields } from "@utils/validateRequiredFields.ts"
 import { SUCCESS_CODE } from "@successHandler/configs.successHandler.ts"
+import { ERROR_CODE } from "@errorHandler/configs.errorHandler.ts"
 import { createdHandler } from "@successHandler/successHandler.ts"
+import type { IUserReqBodyRequest } from "@api/types/user.d.ts"
 import { deleteObjectIds } from "@utils/idCleaner.ts"
+import { processUserData, decryptUserData } from "@utils/crypto.ts"
 import type { Request, Response } from "express"
 import { User } from "@models/index.ts"
-import bcrypt from "bcrypt"
 
 /**
  * Controller for creating a new user.
@@ -22,48 +23,32 @@ const createUser = async (req: Request, res: Response): Promise<void> => {
 	try {
 		// Extract user details from the request body
 		const userObject: IUserReqBodyRequest = req.sanitizedBody
+		// Remove any user IDs from the request body for security reasons
+		const cleanUserObject = deleteObjectIds(userObject)
+		// Validate the presence of required fields in the request body
+		if (!validateRequiredUserFields(cleanUserObject)) {
+			errorHandler(res, ERROR_CODE.MISSING_INFO)
+			return
+		}
 		// Check if the user is exist
 		const userExist = await User.findOne({ email: userObject.email })
-
 		if (userExist) {
 			errorHandler(res, ERROR_CODE.UNABLE_CREATE_USER)
 			return
 		}
 
-		// Remove any user IDs from the request body for security reasons
-		const cleanUserObject = deleteObjectIds(userObject)
-		// Validate the presence of required fields in the request body
-		if (
-			!cleanUserObject.role ||
-			!cleanUserObject.password ||
-			!cleanUserObject.email ||
-			!cleanUserObject.name ||
-			!cleanUserObject.firstName ||
-			!cleanUserObject.birthDate
-		) {
-			errorHandler(res, ERROR_CODE.MISSING_INFO)
-			return
-		}
-		// Hash the user's password
-		const hashedPassword: string = await bcrypt.hash(cleanUserObject.password, 10)
-		delete cleanUserObject.password
 		// Create a new user instance with the hashed password
-		const newUser = new User({
-			email: cleanUserObject.email,
-			password: hashedPassword,
-			name: cleanUserObject.name,
-			firstName: cleanUserObject.firstName,
-			birthDate: cleanUserObject.birthDate,
-			role: "user"
-		})
+		const newUser = await processUserData(cleanUserObject)
 		// Save the new user to the database
 		const savedUser = await newUser.save()
-
 		if (!savedUser) {
 			errorHandler(res, ERROR_CODE.SERVER)
 			return
 		}
-		createdHandler(res, SUCCESS_CODE.USER_CREATED, cleanUserObject)
+		const { password, _id,  ...userWithoutPassword } = savedUser.toObject()
+		const decryptedUserResponse = decryptUserData(userWithoutPassword)
+
+		createdHandler(res, SUCCESS_CODE.USER_CREATED, decryptedUserResponse)
 		return
 	} catch (error) {
 		// Handle unexpected errors
