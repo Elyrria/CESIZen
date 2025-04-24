98776a43b5387add312b011fdbc3d03744dec36a
Quentin <quentindumon352a@gmail.com>
2025-04-24 17:47:49 +0200

    feat(M:RefreshToken): add refreshToken validation rules
    Refs: #54

---
+++ backend/api/src/middlewares/validator/refreshToken.validator.ts
@@ -0,0 +1,11 @@
+import {VALIDATOR} from "@validator/schemas/refreshToken.validator.schema.ts"
+
+/**
+ * Validation rules for refreshToken registration (createRefreshToken).
+ * These rules validate the required fields for creating a new refreshToken.
+ *
+ * @returns An array of validation rules for the 'refreshToken' field.
+ */
+export const createRefreshTokenValidationRules = [
+    ...VALIDATOR.REQUIRED.REFRESH_TOKEN()
+]
