9a4602313fe17e82b8a6257a59dc91454ae96f3c
Quentin <quentindumon352a@gmail.com>
2025-04-24 20:20:01 +0200

    feat(S:RefreshToken): update generateRefreshToken to save more information in DB like userAgent, ipAddress, and call the method to encrypt them
    Refs: #53

--- backend/api/src/utils/generateTokens.ts
+++ backend/api/src/utils/generateTokens.ts
@@ -1,6 +1,8 @@
-import type { IUserToken } from "@api/types/tokens.d.ts"
+import type { IUserToken, IRefreshTokenCreate } from "@api/types/tokens.d.ts"
+import { processRefreshToken } from "@utils/crypto.ts"
 import { CONFIGS } from "@configs/global.configs.ts"
 import { RefreshToken } from "@models/index.ts"
+import type { Request } from "express"
 import jwt from "jsonwebtoken"
 
 /**
@@ -35,16 +37,37 @@ export const generateAccesToken = (user: IUserToken): string | undefined => {
  * @param {IUserToken} user - The user object containing the user ID and role.
  * @returns {Promise<string | undefined>} - The generated refresh token, or undefined if the secret key is not defined.
  */
-export const generateRefreshToken = async (user: IUserToken): Promise<string | undefined> => {
+export const generateRefreshToken = async (user: IUserToken, req: Request): Promise<string | undefined> => {
 	const secretKey: string | undefined = CONFIGS.TOKEN_SECRET.KEY
 
 	if (!secretKey) {
 		throw new Error("Missing secret key")
 	}
+	// Get client IP address
+	const clientIp: string =
+		req.ip ||
+		(Array.isArray(req.headers["x-forwarded-for"])
+			? req.headers["x-forwarded-for"][0]
+			: req.headers["x-forwarded-for"]) ||
+		req.socket.remoteAddress ||
+		"unknown"
+	// Get User-Agent
+	const userAgent: string = Array.isArray(req.headers["user-agent"])
+		? req.headers["user-agent"][0]
+		: req.headers["user-agent"] || "unknown"
 
 	const refreshToken = jwt.sign({ userId: user.id, role: user.role }, secretKey, { expiresIn: "7d" })
 
-	await RefreshToken.create({ refreshToken: refreshToken, userId: user.id })
+	const refreshTokenData: IRefreshTokenCreate = {
+		refreshToken,
+		userAgent,
+		ipAddress: clientIp,
+		userId: user.id,
+	}
+
+	const newRefreshToken: IRefreshTokenCreate = await processRefreshToken(refreshTokenData)
+
+	await RefreshToken.create(newRefreshToken)
 
 	return refreshToken
 }
