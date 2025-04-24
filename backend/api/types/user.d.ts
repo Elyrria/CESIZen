50c320dd3d929b970fe0a3dcdde5079f9ba35b1b
Quentin <quentindumon352a@gmail.com>
2025-04-24 11:03:47 +0200

    refactor(S:Back): update user interface to be more modular
    Refs: #24

--- backend/api/types/user.d.ts
+++ backend/api/types/user.d.ts
@@ -4,25 +4,41 @@ import type { Role } from "./roles.d.ts"
  * Base interface for a user
  */
 export interface IUser {
-    /** User's unique email */
-    email?: string;
-    /** User's hashed password */
-    password?: string;
-    /** User's name (required) */
-    name: string;
-    /** User's first name */
-    firstName?: string;
-    /** User's birth date in the system */
-    birthDate?: DATE;
-    /** User's role in the system */
-    role?: Role;
-    /** Account creation date */
-    createdAt?: Date;
-    /** Account last update date */
-    updatedAt?: Date;
-    /** Account last update date */
-    active?: Boolean;
+	/** User's unique email */
+	email: string
+	/** User's hashed password */
+	password: string
+	/** User's name (required) */
+	name: string
+	/** User's first name */
+	firstName: string
+	/** User's birth date in the system */
+	birthDate: DATE
+	/** User's role in the system */
+	role: Role
+	/** Account creation date */
+	createdAt: Date
+	/** Account last update date */
+	updatedAt: Date
+	/** Account last update date */
+	active: Boolean
 }
+
+export type IUserCreate = Pick<IUser, "email" | "password" | "name" | "firstName" | "birthDate"> &
+	Partial<Pick<IUser, "role">>
+
+export type IUserUpdate = Partial<Omit<IUser, "createdAt" | "updatedAt">>
+
+export type IUserDisplay = Omit<IUser, "password"> & { _id: string | Types.ObjectId }
+
+// export interface IUserDataCrypto {
+// 	email: string
+// 	password: string
+// 	name: string
+// 	firstName: string
+// 	birthDate: Date
+// 	role: Role
+// }
 
 /**
  * Extended interface for user-related requests
@@ -46,3 +62,10 @@ export interface IUserReqBodyRequest extends IUser {
  * Extends the IUser interface with Mongoose Document properties
  */
 export interface IUserDocument extends IUser, Document {}
+
+export interface IUserDataToHash {
+	password: string
+	name: string
+	firstName: string
+	[key: string]: string | number | boolean | null | undefined
+}
