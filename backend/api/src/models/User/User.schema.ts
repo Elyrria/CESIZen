376fcc8a7eb8a8dcc70b29379bd42aa0a3493b54
Quentin <quentindumon352a@gmail.com>
2025-04-24 11:09:27 +0200

    refactor(M:User): update model to remove schema validations (handled by validators middleware)
    Refs: #24

--- backend/api/src/models/User/User.schema.ts
+++ backend/api/src/models/User/User.schema.ts
@@ -23,41 +23,17 @@ const userSchema = new Schema<IUserDocument>(
 			type: String,
 			required: [true, USER_MESSAGE.required(FIELD.PASSWORD)],
 			trim: true,
-			minlength: [
-				CONFIG_FIELD.LENGTH.PASSWORD.MIN,
-				USER_MESSAGE.minLength(FIELD.PASSWORD, CONFIG_FIELD.LENGTH.PASSWORD.MIN),
-			],
-			maxlength: [
-				CONFIG_FIELD.LENGTH.PASSWORD.MAX,
-				USER_MESSAGE.maxLength(FIELD.PASSWORD, CONFIG_FIELD.LENGTH.PASSWORD.MAX),
-			],
 			select: false, // Prevents password from being included in query results by default
 		},
 		name: {
 			type: String,
 			required: [true, USER_MESSAGE.required(FIELD.NAME)],
 			trim: true,
-			minlength: [
-				CONFIG_FIELD.LENGTH.NAME.MIN,
-				USER_MESSAGE.minLength(FIELD.NAME, CONFIG_FIELD.LENGTH.NAME.MIN),
-			],
-			maxlength: [
-				CONFIG_FIELD.LENGTH.NAME.MAX,
-				USER_MESSAGE.maxLength(FIELD.NAME, CONFIG_FIELD.LENGTH.NAME.MAX),
-			],
 		},
 		firstName: {
 			type: String,
 			required: [true, USER_MESSAGE.required(FIELD.FIRST_NAME)],
 			trim: true,
-			minlength: [
-				CONFIG_FIELD.LENGTH.FIRST_NAME.MIN,
-				USER_MESSAGE.minLength(FIELD.FIRST_NAME, CONFIG_FIELD.LENGTH.FIRST_NAME.MIN),
-			],
-			maxlength: [
-				CONFIG_FIELD.LENGTH.FIRST_NAME.MAX,
-				USER_MESSAGE.maxLength(FIELD.FIRST_NAME, CONFIG_FIELD.LENGTH.FIRST_NAME.MAX),
-			],
 		},
 		role: {
 			type: String,
@@ -69,14 +45,8 @@ const userSchema = new Schema<IUserDocument>(
 			default: ROLE_HIERARCHY[ROLE_HIERARCHY.length - 1], // Sets the default role to the lowest privilege
 		},
 		birthDate: {
-			type: Date,
+			type: String,
 			required: [true, USER_MESSAGE.required(FIELD.BIRTH_DATE)],
-			validate: {
-				validator: function (value: Date) {
-					return value < new Date() // Ensures birth date is in the past
-				},
-				message: "Birth date cannot be in the future",
-			},
 		},
 		active: {
 			type: Boolean,
@@ -89,13 +59,6 @@ const userSchema = new Schema<IUserDocument>(
 		toObject: { virtuals: true }, // Includes virtual properties when converting to objects
 	}
 )
-/**
- * Virtual property that combines firstName and lastName
- * Not stored in the database but calculated on demand
- */
-userSchema.virtual("fullName").get(function () {
-	return `${this.firstName} ${this.name}`
-})
 /**
  * Optimize query performance with additional indexes
  */
