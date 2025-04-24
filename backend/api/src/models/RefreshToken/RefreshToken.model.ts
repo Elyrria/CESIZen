24c263d06c776a32fadde893d689d208265e7453
Quentin <quentindumon352a@gmail.com>
2025-04-24 17:48:40 +0200

    chore(S:Back): update import paths
    Refs: #54

--- backend/api/src/models/RefreshToken/RefreshToken.model.ts
+++ backend/api/src/models/RefreshToken/RefreshToken.model.ts
@@ -1,5 +1,5 @@
 import type { IRefreshToken } from "@models/RefreshToken/RefreshToken.interface.ts"
-import {refreshTokenSchema} from "@models/RefreshToken/RefreshToken.schema.ts"
+import refreshTokenSchema from "@models/RefreshToken/RefreshToken.schema.ts"
 import mongoose from "mongoose"
 
 const RefreshToken = mongoose.model<IRefreshToken>("RefreshToken", refreshTokenSchema)
--- backend/api/src/routes/refreshToken/post/createRefreshToken.routes.ts
+++ backend/api/src/routes/refreshToken/post/createRefreshToken.routes.ts
@@ -1,9 +1,9 @@
-// import { refreshTokenValidationRule } from "../middleware/validators/refreshTokenValidator.ts"
-// import { validationErrorHandler } from "../middleware/validationErrorHandler.ts"
+import { createRefreshTokenValidationRules } from "@validator/refreshToken.validator.ts"
+import { validationErrorHandler } from "@validator/validationError.validator.ts"
 import { refreshToken } from "@controllers/auth/refreshToken.controller.ts"
 import { Router } from "express"
 const createRefreshTokenRouter = Router()
 // POST /api/v1/refresh-token/create
-createRefreshTokenRouter.post("/create", refreshToken)
+createRefreshTokenRouter.post("/create", createRefreshTokenValidationRules, validationErrorHandler, refreshToken)
 
 export default createRefreshTokenRouter
