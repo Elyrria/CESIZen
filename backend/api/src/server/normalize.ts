62d125de45352ca55772050fceb38cba29adcbe2
Quentin <quentindumon352a@gmail.com>
2025-05-05 19:04:24 +0200

    refactor(S:Back): update normalize.ts to be more readable
    Refs: #91

--- backend/api/src/server/normalize.ts
+++ backend/api/src/server/normalize.ts
@@ -6,16 +6,15 @@
  * @returns {string | number | false} - The normalized port.
  * @throws {Error} - If the port value is less than or equal to 0.
  */
-export const normalize = (portBackend: string): string | number | false => {
+export const normalize = (portBackend: string): string | number => {
 	const port = parseInt(portBackend, 10) // Convert port in integer
-	switch (true) {
-		case isNaN(port):
-			return portBackend // Return the original string if not a number
-		case port <= 0:
-			throw new Error(`Value cannot be ≤ to 0: ${port}`)
-		case port > 0:
-			return port // Return the parsed integer if valid
-		default:
-			return false // Fallback, though this should never happen with the above conditions
+	if (isNaN(port)) {
+		return portBackend
 	}
+
+	if (port <= 0) {
+		throw new Error(`Value cannot be ≤ to 0: ${port}`)
+	}
+
+	return port
 }
