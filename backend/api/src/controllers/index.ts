fc01b80803b0f11829383ed510dbfb00e6d311e8
Quentin <quentindumon352a@gmail.com>
2025-05-12 12:06:16 +0200

    feat(R:User): update import type and add import loginUser
    Refs: #44

--- backend/api/src/controllers/index.ts
+++ backend/api/src/controllers/index.ts
@@ -1,6 +1,5 @@
 // Usser controllers
-import { default as createUser } from "@controllers/user/createUser.controller.ts"
+export { createUser } from "@controllers/user/createUser.controller.ts"
+export { loginUser } from "@controllers/user/loginUser.controller.ts"
 // Activity controllers
 // Infroamtion controllers
-
-export default createUser
