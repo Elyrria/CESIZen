1db2c352f42e1290f29cef1bca5ef8df8b2fac02
Quentin <quentindumon352a@gmail.com>
2025-05-12 18:12:23 +0200

    feat(C:Users): add to index.ts the deleteUser controller
    Refs: #24

--- backend/api/src/controllers/index.ts
+++ backend/api/src/controllers/index.ts
@@ -1,8 +1,9 @@
 // Usser controllers
 export { adminCreateUser } from "@controllers/user/adminCreateUser.controller.ts"
+export { deleteUserById } from "@controllers/user/deleteUser.controller.ts"
 export { createUser } from "@controllers/user/createUser.controller.ts"
+export { logoutUser } from "@controllers/user/logoutUser.controller.ts"
 export { getAllUsers } from "@controllers/user/getUsers.controller.ts"
 export { loginUser } from "@controllers/user/loginUser.controller.ts"
-export { logoutUser } from "@controllers/user/logoutUser.controller.ts"
 // Activity controllers
 // Infroamtion controllers
