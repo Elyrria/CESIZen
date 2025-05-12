288619edda7ad8498affd137aa0fb0a9995b9c20
Quentin <quentindumon352a@gmail.com>
2025-05-12 16:37:27 +0200

    feat(R:User): add to controllers index.ts getAllusers.ts
    Refs: #44

--- backend/api/src/controllers/index.ts
+++ backend/api/src/controllers/index.ts
@@ -1,6 +1,7 @@
 // Usser controllers
 export { adminCreateUser } from "@controllers/user/adminCreateUser.controller.ts"
 export { createUser } from "@controllers/user/createUser.controller.ts"
+export { getAllUsers } from "@controllers/user/getUsers.controller.ts"
 export { loginUser } from "@controllers/user/loginUser.controller.ts"
 // Activity controllers
 // Infroamtion controllers
