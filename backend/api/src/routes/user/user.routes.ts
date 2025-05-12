49e3901b5dfa07486e9da122e9dec692b419751b
Quentin <quentindumon352a@gmail.com>
2025-05-12 14:07:20 +0200

    feat(R:User): connect adminCreateUserRouter to main user router
    Refs: #44

--- backend/api/src/routes/user/user.routes.ts
+++ backend/api/src/routes/user/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
 // Import POST routes
+import adminCreateUseRouter from "@routes/user/post/adminCreateUser.route.ts"
 import createUseRouter from "@routes/user/post/createUser.route.ts"
 import loginUseRouter from "@routes/user/post/loginUser.route.ts"
 
@@ -10,7 +11,7 @@ import loginUseRouter from "@routes/user/post/loginUser.route.ts"
 // Import PUT routes
 
 const router = Router()
-
-router.use("/v1/users", createUseRouter, loginUseRouter)
+// POST routes
+router.use("/v1/users", createUseRouter, loginUseRouter, adminCreateUseRouter)
 
 export default router
