412f5bcc88da44db1d03dc06d74dc5601ed76a46
Quentin <quentindumon352a@gmail.com>
2025-05-12 12:24:14 +0200

    refactor(R:User): remove auth from login as it is not necessary
    Refs: #24

--- backend/api/src/routes/user/post/loginUser.route.ts
+++ backend/api/src/routes/user/post/loginUser.route.ts
@@ -1,11 +1,10 @@
 import { validationErrorHandler } from "@validator/validationError.validator.ts"
 import { loginUserValidationRules } from "@validator/user.validator.ts"
-import { auth } from "@middlewares/security/auth.middleware.ts"
 import { loginUser } from "@controllers/index.ts"
 import { Router } from "express"
 
 const loginUseRouter = Router()
 
-loginUseRouter.post("/login", auth, loginUserValidationRules, validationErrorHandler, loginUser)
+loginUseRouter.post("/login", loginUserValidationRules, validationErrorHandler, loginUser)
 
 export default loginUseRouter
