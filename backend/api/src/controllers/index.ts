521febee193c4480483446729ddf0e9183222d06
Quentin <quentindumon352a@gmail.com>
2025-05-12 14:06:14 +0200

    feat(R:User): connect adminCreateUser route to index.ts
    Refs: #44

--- backend/api/src/controllers/index.ts
+++ backend/api/src/controllers/index.ts
@@ -1,4 +1,5 @@
 // Usser controllers
+export { adminCreateUser } from "@controllers/user/adminCreateUser.controller.ts"
 export { createUser } from "@controllers/user/createUser.controller.ts"
 export { loginUser } from "@controllers/user/loginUser.controller.ts"
 // Activity controllers
