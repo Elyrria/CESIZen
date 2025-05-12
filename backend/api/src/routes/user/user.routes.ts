7669396871d506d54c4751bb242306ef29c3be75
Quentin <quentindumon352a@gmail.com>
2025-05-12 16:39:55 +0200

    feat(R:User): connect user route for getAllUsers
    Refs: #44

--- backend/api/src/routes/user/user.routes.ts
+++ backend/api/src/routes/user/user.routes.ts
@@ -3,9 +3,8 @@ import { Router } from "express"
 import adminCreateUseRouter from "@routes/user/post/adminCreateUser.route.ts"
 import createUseRouter from "@routes/user/post/createUser.route.ts"
 import loginUseRouter from "@routes/user/post/loginUser.route.ts"
-
 // Import GET routes
-
+import getAllUsersRouter from "@routes/user/get/getAllUsers.route.ts"
 // Import DELETE routes
 
 // Import PUT routes
@@ -13,5 +12,8 @@ import loginUseRouter from "@routes/user/post/loginUser.route.ts"
 const router = Router()
 // POST routes
 router.use("/v1/users", createUseRouter, loginUseRouter, adminCreateUseRouter)
-
+// GET routes
+router.use("/v1/users", getAllUsersRouter)
+// PUT routes
+// DETLE routes
 export default router
