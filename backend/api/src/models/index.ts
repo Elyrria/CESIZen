05ab259d320171d54ab49c7f1ba2fc9fdc59ede8
Quentin <quentindumon352a@gmail.com>
2025-05-13 16:00:45 +0200

    feat(M:Information): conncect information model to index.ts
    Refs: #27

--- backend/api/src/models/index.ts
+++ backend/api/src/models/index.ts
@@ -1,2 +1,3 @@
 export { default as RefreshToken } from "@models/RefreshToken/RefreshToken.model.ts"
+export { default as Information } from "@models/Information/Information.model.ts"
 export { default as User } from "@models/User/User.model.ts"
