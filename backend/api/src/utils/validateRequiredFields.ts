9c52e6a519a239fd0ebf217ec7f1b33738cac7ef
Quentin <quentindumon352a@gmail.com>
2025-05-13 16:01:55 +0200

    feat(C:Informations): implement required information fields validation method
    Refs: #27

--- backend/api/src/utils/validateRequiredFields.ts
+++ backend/api/src/utils/validateRequiredFields.ts
@@ -1,5 +1,11 @@
+import type { IInformationDocument } from "@api/types/information.d.ts"
+
 import type { IUserCreate } from "@api/types/user.d.ts"
 // Helper function for validating required fields
 export function validateRequiredUserFields(user: Partial<IUserCreate>): boolean {
 	return Boolean(user.role && user.password && user.email && user.name && user.firstName && user.birthDate)
+}
+
+export function validateRequierdInformationFields(information: Partial<IInformationDocument>): boolean {
+	return Boolean(information.title && information.description && information.name && information.type)
 }
