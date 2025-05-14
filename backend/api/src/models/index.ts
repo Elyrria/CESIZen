67a4d98ee57c400daf0e91f203faa8c94ac846f6
Quentin <quentindumon352a@gmail.com>
2025-05-14 16:34:59 +0200

    feat(M:Category): connect category model to index.ts
    Refs: #30

--- backend/api/src/models/index.ts
+++ backend/api/src/models/index.ts
@@ -1,3 +1,4 @@
 export { default as RefreshToken } from "@models/RefreshToken/RefreshToken.model.ts"
 export { default as Information } from "@models/Information/Information.model.ts"
+export {default as Category} from "@models/Category/Category.model.ts"
 export { default as User } from "@models/User/User.model.ts"
