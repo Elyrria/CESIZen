151c0eb39a5746c88f284023453a8bfd4b37ae65
Quentin <quentindumon352a@gmail.com>
2025-05-14 16:34:34 +0200

    refactor(M:Category): update name file and change import path
    Refs: #30

--- backend/api/src/models/Category/category.model.ts
+++ backend/api/src/models/Category/category.model.ts
@@ -1,6 +1,8 @@
 import type { ICategoryDocument } from "@api/types/category.d.ts"
-import categorySchema from "@models/Category/category.schema.ts"
+import categorySchema from "@models/Category/Category.schema.ts"
 import mongoose from "mongoose"
 
-// Définition du modèle Category
-export const Category = mongoose.model<ICategoryDocument>("Category", categorySchema)
+
+const Category = mongoose.model<ICategoryDocument>("Category", categorySchema)
+
+export default Category
