72c8350bdedcf1155db634f9b3de8778e6d4ad3b
Quentin <quentindumon352a@gmail.com>
2025-05-14 15:33:11 +0200

    feat(R:Information): connect getPublicInformation router to information router
    Refs: #45

--- backend/api/src/routes/information/information.route.ts
+++ backend/api/src/routes/information/information.route.ts
@@ -4,6 +4,7 @@ import createInformationRouter from "@routes/information/post/createInformation.
 import updateInformatioNRouter from "@routes/information/put/updateInformation.route.ts"
 // Import GET routes
 import getInformationsRouter from "@routes/information/get/getInformations.route.ts"
+import getPublicInformationsRouter from "./get/getPublicInformations.route.ts"
 import getMediaByIdRouter from "@routes/information/get/getMediaById.route.ts"
 // Import DELETE routes
 import deleteInformationRouter from "@routes/information/delete/deleteInformation.route.ts"
@@ -13,7 +14,7 @@ const router = Router()
 // POST routes
 router.use("/v1/informations", createInformationRouter)
 // GET routes
-router.use("/v1/informations", getInformationsRouter, getMediaByIdRouter)
+router.use("/v1/informations", getInformationsRouter, getMediaByIdRouter, getPublicInformationsRouter)
 // DETLE routes
 router.use("/v1/informations", deleteInformationRouter)
 // PUT routes
