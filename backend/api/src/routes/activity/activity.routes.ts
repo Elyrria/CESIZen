c754e205d6e10b8b6d6115c1a4b7c56997dcbe43
Quentin <quentindumon352a@gmail.com>
2025-05-15 17:34:22 +0200

    feat(R:Activity): connect activity routes to activity router
    Refs: #47

--- backend/api/src/routes/activity/activity.routes.ts
+++ backend/api/src/routes/activity/activity.routes.ts
@@ -1,16 +1,22 @@
 // Import POST routes
 import createActivityRouter from "@routes/activity/post/createActivity.route.ts"
 // Import PUT routes
+import updateActivityRouter from "@routes/activity/put/updateActivity.route.ts"
 // Import GET routes
+import getActivitiesRouter from "@routes/activity/get/getActivities.route.ts"
+import getPublicActivitiesRouter from "@routes/activity/get/getPublicActivities.route.ts"
 // Import DELETE routes
+import deleteActivityRouter from "@routes/activity/delete/deleteActivity.route.ts"
 import { Router } from "express"
 
 const router = Router()
+// PUT routes
+router.use("/v1/activities", updateActivityRouter)
 // POST routes
 router.use("/v1/activities", createActivityRouter)
 // GET routes
+router.use("/v1/activities", getActivitiesRouter, getPublicActivitiesRouter)
 // DETLE routes
-
-// PUT routes
+router.use("/v1/activities", deleteActivityRouter)
 
 export default router
