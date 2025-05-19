c1abff6760218b242a4bc85bc2752637e0bbbe63
Quentin <quentindumon352a@gmail.com>
2025-05-19 20:55:03 +0200

    feat(S:Frontend): create AuthInitializer to load user profile from authentication cookie
    Refs: #70

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import AuthInitializer from "@/components/auth/AuthInitializer"
+import RegisterPage from "./pages/auth/RegisterPage"
 import LoginPage from "@pages/auth/LoginPages"
 import Layout from "@/components/Layout"
 
 const App: React.FC = () => {
 	return (
 		<Router>
-			<Layout>
-				<Routes>
-					<Route path='/login' element={<LoginPage />} />
-				</Routes>
-			</Layout>
+			<AuthInitializer>
+				<Layout>
+					<Routes>
+						<Route path='/login' element={<LoginPage />} />
+						<Route path='/register' element={<RegisterPage />} />
+						{/* Autres routes */}
+					</Routes>
+				</Layout>
+			</AuthInitializer>
 		</Router>
 	)
 }
