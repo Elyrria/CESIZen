3b96d6f1dbb82dba0a1caa04302234c7c20ec0d1
Quentin <quentindumon352a@gmail.com>
2025-05-21 19:56:00 +0200

    feat(S:Front): update ImageInformation class and it types
    Refs: #82

--- frontend/src/factories/ImageInformation.ts
+++ frontend/src/factories/ImageInformation.ts
@@ -1,29 +1,29 @@
-import type { Information, InformationStatus, InformationType } from "@/types/factory"
-
-export class ImageInformation implements Information {
-	id?: string
-	title: string
-	descriptionInformation: string
-	name: string
-	type: InformationType
-	status: InformationStatus
-	authorId: string
-	categoryId: string[] | { _id: string; name: string; id: string }[]
-	validatedAndPublishedAt?: string | null
-	validatedBy?: string | null
-	fileId?: string
+import type { IInformation, InformationType, InformationStatus } from "@/factories/Factory"
+export class ImageInformation implements IInformation {
 	fileMetadata?: {
 		filename: string
 		contentType: string
 		size: number
 		uploadDate: string
 	}
+	_id?: string
+	title: string
+	descriptionInformation: string
+	name: string
+	type: InformationType
+	status: InformationStatus
+	authorId: string | { _id: string; name: string; id: string }
+	categoryId: string[] | { _id: string; name: string; id: string }[]
+	validatedAndPublishedAt?: string | null
+	validatedBy?: string | null
+	fileId?: string
 	createdAt?: string
 	updatedAt?: string
 	mediaUrl?: string
+	id: string
 
-	constructor(data: Partial<Information>) {
-		this.id = data.id
+	constructor(data: Partial<IInformation>) {
+		this.id = data.id || data._id || ""
 		this.title = data.title || ""
 		this.descriptionInformation = data.descriptionInformation || ""
 		this.name = data.name || ""
