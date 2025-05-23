8e61f5abcda469537a0b9fb0363b8b1c075ec085
Quentin <quentindumon352a@gmail.com>
2025-05-23 09:56:30 +0200

    feat(S:Front): connect homePage and add lazy import with suspense
    Refs: #82

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import "react-toastify/dist/ReactToastify.css"
+
 import AuthInitializer from "@/components/auth/AuthInitializer"
-import RegisterPage from "./pages/auth/RegisterPage"
-import LoginPage from "@pages/auth/LoginPages"
-import Layout from "@/components/Layout"
+import Layout from "@/layouts/Layout"
+
+import React, { Suspense, lazy } from "react"
+
+const RegisterPage = lazy(() => import("@/pages/auth/RegisterPage"))
+const LoginPage = lazy(() => import("@/pages/auth/LoginPages"))
+const HomePage = lazy(() => import("@/pages/home/HomePage"))
 
 const App: React.FC = () => {
 	return (
 		<Router>
 			<AuthInitializer>
 				<Layout>
-					<Routes>
-						<Route path='/login' element={<LoginPage />} />
-						<Route path='/register' element={<RegisterPage />} />
-						{/* Autres routes */}
-					</Routes>
+					<Suspense
+						fallback={
+							<div className='min-h-screen flex items-center justify-center'>
+								<div className='animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-fr-blue'></div>
+							</div>
+						}
+					>
+						<Routes>
+							{/* Routes publiques */}
+							<Route path='/register' element={<RegisterPage />} />
+							<Route path='/login' element={<LoginPage />} />
+							<Route path='/' element={<HomePage />} />
+							{/* Redirection par défaut */}
+							<Route path='*' element={<Navigate to='/' replace />} />
+						</Routes>
+					</Suspense>
 				</Layout>
 			</AuthInitializer>
 		</Router>
